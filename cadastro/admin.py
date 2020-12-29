209c8a5cfe79b06de8d1c7d3d4bdc2c97450c072
Rafael <noprafa@gmail.dom>
2020-12-29 14:10:00 -0300

    API Keys inseridas no cadastro com OneToOne relationship

--- cadastro/admin.py
+++ cadastro/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from cadastro.models import Curriculum, Vagas
+from cadastro.models import Curriculum, Vagas, ManageAPIKey
 
 # Register your models here.
 
 # admin.site.register(Cadastro)
 admin.site.register(Curriculum)
 admin.site.register(Vagas)
+admin.site.register(ManageAPIKey)

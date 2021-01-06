5646790ce4a3b67ed416fb90f77ccf931fc179f1
Rafael <noprafa@gmail.dom>
2021-01-06 13:45:56 -0300

    Migrate to Django

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+Release: python manage.py migrate
 web: gunicorn djangoProject.wsgi --log-file -
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -127,8 +127,8 @@ DATABASES = {
         # 'NAME': "postgres",
         # 'USER': 'postgres',
         # 'PASSWORD': '123',
-        # 'HOST': '127.0.0.1',
-        # 'PORT': '5432',
+        # 'HOST': '127.0.0.1'
+        #'PORT': '5432',
     }
 }
 
